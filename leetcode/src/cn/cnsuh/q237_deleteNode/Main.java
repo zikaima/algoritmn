10da3e775c27c816455ea359cf89bcea8daed696
Magic-Ma <cnsuh@qq.com>
2020-07-05 23:03:52 +0800

    q237

---
+++ leetcode/src/cn/cnsuh/q237_deleteNode/Main.java
@@ -0,0 +1,26 @@
+package cn.cnsuh.q237_deleteNode;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+
+
+
+    }
+
+    static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
